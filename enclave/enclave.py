1bd4de3a391d6709cd1bbe0315134d08ece82277
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2021-10-02 09:30:18 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -71,13 +71,13 @@ class enclave(commands.Cog):
 
     @commands.command()
     async def тест(self, ctx):
-        #artj=discord.utils.get(ctx.guild.roles, id=893293699704975360)
-        #oldart=0
+        artj=discord.utils.get(ctx.guild.roles, id=893293699704975360)
+        oldart=0
         #if art==0:
         #    while artj.name.startswith=="Артефакты":
         #curr="Артефакты: "+str(oldart)
         #        if artj.name==curr:
-        await ctx.send("test")
+        await ctx.send(artj.name+" = "+str(oldart))
         #            return artj.edit(name="Артефакты: "+str(oldart+1))
         #        oldart+=1
 
