e436a993d6bdc79623cffabc8583b069efb461af
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2021-11-05 08:58:35 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -1213,7 +1213,7 @@ class enclave(commands.Cog):
         if len(choices) < 2:
             await ctx.send("Слишком мало вариков!")
         else:
-            await ctx.send(random.choice(choices, k=2))
+            await ctx.send(random.choices(choices, k=2))
 
     @commands.command()
     async def скажи(self, ctx, room: discord.TextChannel = None, *, text):
