64141a87f8b0cb974dd94fef9abaccb69733f40d
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2021-11-06 13:57:59 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -1528,7 +1528,7 @@ class enclave(commands.Cog):
                 P8="⬆"
             P9=P6
             P6=P3
-            if i==(j-1):
+            if i==(j-1) and authbal<200:
                 roll=[P4, P5]
                 P3=random.choice(roll)
             else:
