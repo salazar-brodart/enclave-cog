f22d1e5d7c0a0d597fe21b7d09e5150ee4815952
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2021-11-10 23:24:50 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -1252,7 +1252,8 @@ class enclave(commands.Cog):
     @commands.command()
     async def уборка(self, ctx, i: int = 1):
         GOB=discord.utils.get(ctx.guild.roles, id=583993057330855946)
-        if GOB not in ctx.author.roles:
+        DES=discord.utils.get(ctx.guild.roles, id=903847910951751692)
+        if GOB not in ctx.author.roles and DES not in ctx.author.roles:
             return await ctx.send(f"*{ctx.author.display_name} подметает полы.*")
         j=0
         async for mes in ctx.message.channel.history(limit=i,oldest_first=False):
