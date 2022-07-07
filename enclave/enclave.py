637e4f9a27c1226be76cde692b3768ae5a9b307b
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2022-07-07 13:51:14 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -72,8 +72,7 @@ class enclave(commands.Cog):
     @commands.command()
     async def тест1(self, ctx: Context):
         author = ctx.author
-        guild=ctx.message.channel.guild
-        BAR=await guild.create_role(name='Квест Ремесло: ❌❌❌❌❌❌❌❌❌❌❌❌')
+        BAR=await ctx.guild.create_role(name='Квест Ремесло: ❌❌❌❌❌❌❌❌❌❌❌❌')
         await author.add_roles(BAR)
         return await ctx.send(f"*{author.display_name} получает роль {BAR.name}*")
         
