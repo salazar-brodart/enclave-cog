082365cda23ee503b2401fcc470a9286735d4317
salazar-brodart <85242430+salazar-brodart@users.noreply.github.com>
2023-01-22 21:19:41 +0700

    Update enclave.py

--- enclave/enclave.py
+++ enclave/enclave.py
@@ -136,7 +136,7 @@ class enclave(commands.Cog):
         await ctx.send(str(command)+"   "+str(com))
         emdeb=discord.Embed(title = f'{user.display_name} выбирай категорию.', colour=discord.Colour.random())
         await ctx.send(embed=embed, components = [
-                Button(style = ButtonStyle.blurple, label = 'Простые, 1 балл')]
+                Button(style = ButtonStyle.blurple, label = 'Простые, 1 балл')])
 
     @это.command(name="иллюзия")
     async def это_иллюзия(self, ctx: Context, user: discord.Member = None):
